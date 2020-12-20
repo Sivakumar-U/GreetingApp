f9f0a0270fa72353121e44cd39e8b057ee9d8a5c
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-20 21:22:24 +0530

    Added: code to give greeting message with user first name and last name

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -5,10 +5,13 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bridgelabz.greetingapp.model.Greeting;
+import com.bridgelabz.greetingapp.model.User;
 import com.bridgelabz.greetingapp.service.IGreetingService;
 
 @RestController
@@ -33,6 +36,11 @@ public class GreetingController {
 	@GetMapping("/message")
 	public String message() {
 		return greetingService.greet();
+	}
+	
+	@PostMapping("/fullName")
+	public String sayWow(@RequestBody User user) {
+		return "Hello "+user.firstName+" "+user.lastName;
 	}
 
 }
