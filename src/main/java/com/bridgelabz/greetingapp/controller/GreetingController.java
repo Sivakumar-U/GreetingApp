d2550b7822e00a2060b5572532fc70e85cb98a99
Sivakumar-U <usivakumarreddy@gmail.com>
2020-12-21 19:12:35 +0530

    Added: Code to edit a greeting messages in the repository

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,7 +1,13 @@
 package com.bridgelabz.greetingapp.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,11 +23,33 @@ public class GreetingController {
 	@Autowired
 	private IGreetingService iGreetingService;
 
+	User user = new User();
+
 	@GetMapping(value = { "/home" })
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		User user = new User();
 		user.setFirstName(name);
 		return iGreetingService.addGreeting(user);
+	}
+
+	@PostMapping("/post")
+	public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		user.setFirstName(name);
+		return iGreetingService.addGreeting(user);
+	}
+
+	@GetMapping("/getGreeting/{id}")
+	public Optional<Greeting> getGreetingById(@PathVariable(value = "id") long id) {
+		return iGreetingService.getGreetingById(id);
+	}
+
+	@GetMapping("/getAllGreetings")
+	public List<Greeting> getAllGreeting() {
+		return iGreetingService.getAllGreetings();
+	}
+
+	@PutMapping("/editGreeting/{id}")
+	public Optional<Greeting> editGreetingById(@PathVariable("id") long id, @RequestParam(value = "name") String name) {
+		return iGreetingService.editGreetingById(id, name);
 	}
 
 }
